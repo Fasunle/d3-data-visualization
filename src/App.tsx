d31effcab7489a7553518b86ac4fee71e199272b
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-02-19 14:01:42 +0100

    extracted the Eye into its component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 
 const width = 960;
@@ -10,6 +9,23 @@ const radius = (width/2 < height/2)? width/2 - strokeOffset: height/2- strokeOff
 const eyeRadius = 50;
 const eyeOffsetX = 100;
 const eyeOffsetY = 50;
+
+type EyeType =  {
+  centerX:number;
+  centerY:number;
+  OffsetX:number; 
+  OffsetY:number; 
+  radius:number; 
+  fill:string;
+  right: boolean;
+}
+
+const Eye = (eye:EyeType) => <circle 
+          cx={eye.right ? eye.centerX + eye.OffsetX: eye.centerX - eye.OffsetX } 
+          cy={eye.centerY - eye.OffsetY}
+          r={eye.radius}
+          fill={eye.fill} 
+        />;
 
 function App() {
   return (
@@ -23,17 +39,24 @@ function App() {
           stroke='black' 
           strokeWidth={strokeOffset}
         />
-        <circle 
-          cx={centerX - eyeOffsetX } 
-          cy={centerY - eyeOffsetY}
-          r={eyeRadius}
-          fill="black" 
+        
+        <Eye 
+          OffsetX={eyeOffsetX}
+          OffsetY={eyeOffsetY}
+          centerX={centerX}
+          centerY={centerY}
+          radius={eyeRadius}
+          fill="black"
+          right={false}
         />
-        <circle 
-          cx={centerX + eyeOffsetX } 
-          cy={centerY - eyeOffsetY}
-          r={eyeRadius}
-          fill="black" 
+        <Eye 
+          OffsetX={eyeOffsetX}
+          OffsetY={eyeOffsetY}
+          centerX={centerX}
+          centerY={centerY}
+          radius={eyeRadius}
+          fill="black"
+          right={true}
         />
       </svg>
     </div>
