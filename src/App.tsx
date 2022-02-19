6daeac5772f941811a36a3e279f25ddeba9a5501
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-02-19 13:36:35 +0100

    making a face with svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const width = 960;
+const centerX= width/2;
+const height = 500;
+const centerY= height/2;
+const strokeOffset = 10;
+const radius = (width/2 < height/2)? width/2 - strokeOffset: height/2- strokeOffset;
+const eyeRadius = 50;
+const eyeOffsetX = 100;
+const eyeOffsetY = 50;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg width={width} height={height} >
+        <circle 
+          cx={centerX} 
+          cy={centerY}
+          r={radius}
+          fill="yellow" 
+          stroke='black' 
+          strokeWidth={strokeOffset}
+        />
+        <circle 
+          cx={centerX - eyeOffsetX } 
+          cy={centerY - eyeOffsetY}
+          r={eyeRadius}
+          fill="black" 
+        />
+        <circle 
+          cx={centerX + eyeOffsetX } 
+          cy={centerY - eyeOffsetY}
+          r={eyeRadius}
+          fill="black" 
+        />
+      </svg>
     </div>
   );
 }
