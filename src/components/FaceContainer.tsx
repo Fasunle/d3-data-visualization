67eb6cba1ae33e10ed11f363ec065ccd6670c24b
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-04-07 09:28:14 +0100

    Face can be placed anywhere on the screen if centerX and centerY are given
    - otherwise, it is placed at the center

--- src/components/FaceContainer.tsx
+++ src/components/FaceContainer.tsx
@@ -1,11 +1,13 @@
 import { PropsWithChildren } from "react"
 
-type IFaceContainer = {
+type TFaceContainer = {
     width: number,
-    height: number
+    height: number,
+    centerX?: number,
+    centerY?: number
 }
 
-export const FaceContainer = ({children, height, width }: PropsWithChildren<IFaceContainer>) => (
+export const FaceContainer = ({children, height, width, centerX, centerY }: PropsWithChildren<TFaceContainer>) => (
     <svg width={width} height={height}>
-        <g transform={`translate(${width / 2},${height / 2})`}>{children}</g></svg>
+        <g transform={`translate(${centerX || width / 2},${centerY || height / 2})`}>{children}</g></svg>
 )
