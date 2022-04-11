cbce944350d76f156b89b099f4143a6b6fadb9f7
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-04-11 07:08:33 +0100

    onMouseMove: get mouse coordinate

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@
  * 
  * @returns App component entry point
  */
+import {MouseEvent} from "react";
 
 export const App = () =>  {
+  
+  /**
+   * gets mouse coordinates and set it to local state of the app component
+   * @param event 
+   */
+  const handleMouseMove = (event: MouseEvent<SVGSVGElement>) =>{
+    console.log({x: event.clientX, y:event.clientY});
+  }
+
   return (
     <div className="App">
-      <svg height={500} width={500}>
+      <svg height={500} width={500} onMouseMove={handleMouseMove}>
         <circle cx={250} cy={250} r={10} />
       </svg>
     </div>
