54f86086e0ae83e25d4e19e7e547bc102e6e3f71
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-04-11 06:46:46 +0100

    BugFix: circle attribute is r not 'radius'

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@
 export const App = () =>  {
   return (
     <div className="App">
-      Hello world
+      <svg height={500} width={500}>
+        <circle cx={250} cy={250} r={10} />
+      </svg>
     </div>
   );
 }
