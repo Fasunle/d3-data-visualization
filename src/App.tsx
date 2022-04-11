aef5d41b433b41b091cb4813b206c0b2185b4dd9
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-04-11 07:30:32 +0100

    Optimization: useCallback hook to wrap handleMouseMove
    - handleMouseMove is only called when setMousePosition is called.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * 
  * @returns App component entry point
  */
-import {useState, MouseEvent} from "react";
+import {useState, useCallback ,MouseEvent} from "react";
 
 
 const initialMousePosition = {x: 250, y:250}; // mouse stays ar the center on initial render
@@ -14,12 +14,17 @@ export const App = () =>  {
   const [mousePosition, setMousePosition] = useState(initialMousePosition);
 
   /**
-   * gets mouse coordinates and set it to local state of the app component
+   * gets mouse coordinates and set it to local state of the app component for perfomance gain, the function is wrapped with useCallback.
+   * 
+   * This hook function prevent unnecessary re-render when the component reload because of other effects that are not related to the setMousePosition.
+   * 
+   * Therefore, the component rerender only when setMousePosition is called!
+   * 
    * @param event 
    */
-  const handleMouseMove = (event: MouseEvent<SVGSVGElement>) =>{
+  const handleMouseMove = useCallback((event: MouseEvent<SVGSVGElement>) =>{
     setMousePosition({x: event.clientX, y:event.clientY});  // local mouse state
-  }
+  }, [setMousePosition]);
 
   return (
     <div className="App">
